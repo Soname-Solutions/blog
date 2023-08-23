157a0da833112cf5a3bef7912f16e1809ecdd9e9
Mikita Petukhou <mikita@Mikitas-Air.local>
2023-08-23 21:44:42 +0200

    data ingest class is done

--- E2E-Data-Pipeline/ask_spotify_etl/luigi_tasks.py
+++ E2E-Data-Pipeline/ask_spotify_etl/luigi_tasks.py
@@ -15,7 +15,7 @@ config = Config()
 
 
 class LuigiMaridbTarget():
-    """get luigi DB target to log task state"""
+    """parent class for ETL Luigi tasks"""
 
 
     def get_luigi_target(self, table, control_value):
@@ -37,6 +37,15 @@ class LuigiMaridbTarget():
             control_value=self.control_value
         )
 
+    def get_data_load_id(self, file):
+        sql_get_id = [f"select data_load_id  from etl_control ec where file_name = '{file}'"]
+        connector = MariaDBConnector()
+        with connector:
+            data_load_id = str(connector.execute(sql_get_id)[0][0]) # [(int),] is a return value
+        return data_load_id
+
+
+
 class ETLControlRegistration(LuigiMaridbTarget, luigi.Task):
 
     file = luigi.Parameter()
@@ -51,14 +60,13 @@ class ETLControlRegistration(LuigiMaridbTarget, luigi.Task):
             data_load_id is returned as a control value for the file load"""
 
         sql_register = [f"insert into etl_control (file_name, status) values ('{self.file}', 'in progress')"]
-        sql_get_id = [f"select data_load_id  from etl_control ec where file_name = '{self.file}'"]
+        
 
         connector = MariaDBConnector()
         with connector:
             connector.execute(sql_register)
 
-        with connector:
-            self.control_value += str(connector.execute(sql_get_id)[0][0]) # [(int),] is a return value
+        self.control_value += self.get_data_load_id(self.file)
 
         logger.info(f'{self.file} is registred in etl_control with data_load_id = {self.control_value}')
 
@@ -72,16 +80,19 @@ class LALoadTask(LuigiMaridbTarget, luigi.Task):
     file = luigi.Parameter()
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.file = self.file
         self.control_value = f"{self.file} under id: "
-        self.la_table = f"la_{self.file.split('_')[0]}"
+        self.la_table = f"la_{str(self.file).split('_')[0]}"
 
     def requires(self):
         return ETLControlRegistration(file=self.file)
-    
+
     def run(self):
         file_landing_zone = config.get('path', 'file_landing_zone')
         file_path = os.path.join(file_landing_zone, self.file)
+        data_load_id = self.get_data_load_id(self.file)
+        self.control_value += data_load_id
 
         with open(file_path,'r',encoding='utf-8') as csv_file:
             csv_reader = csv.reader(csv_file,delimiter='#')
@@ -91,14 +102,17 @@ class LALoadTask(LuigiMaridbTarget, luigi.Task):
             db_connector = MariaDBConnector()
             with db_connector:
                 for row in csv_reader:
+                    # wrap each element in double quotes.
+                    # in case double quotes exist in data, double it (escaping) for sql insert.
+                    row_values = ', '.join(f'''"{element.replace('"', '""')}"''' for element in row)
                     insert_query = [f"""
                         INSERT INTO {self.la_table} ({', '.join(header)})
-                        VALUES ({', '.join(f'"{element}"' for element in row)}, '{self.control_value}')
+                        VALUES ({row_values}, '{data_load_id}')
                     """]
                     db_connector.execute(insert_query)
                     counter += 1
-
         logger.info(f"{counter} rows were inserted into {self.la_table}")
+
 
         super().run()
 
